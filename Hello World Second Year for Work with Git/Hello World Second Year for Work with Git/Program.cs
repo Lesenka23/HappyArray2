8203cf1ee4f2a5333b4e28a478cc0a04772e4be9
Lesya Danko <lesyadankoit@gmail.com>
2024-10-10 09:43:14 -0400

    First Commit

---
+++ Hello World Second Year for Work with Git/Hello World Second Year for Work with Git/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_World_Second_Year_for_Work_with_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
