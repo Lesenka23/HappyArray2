83ea71e52f4723928df7e5b6226f99570d12fe97
Lesya Danko <lesyadankoit@gmail.com>
2024-10-11 15:43:28 -0400

    added a variable name line 12, also What is your name line 11

--- Hello World Second Year for Work with Git/Hello World Second Year for Work with Git/Program.cs
+++ Hello World Second Year for Work with Git/Hello World Second Year for Work with Git/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Hello_World_Second_Year_for_Work_with_Git
+﻿using System.Threading.Channels;
+
+namespace Hello_World_Second_Year_for_Work_with_Git
 {
     internal class Program
     {
@@ -6,6 +8,9 @@
         {
             Console.WriteLine("Hello, World! ");
             Console.WriteLine("Hello, Second Year! ");
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
