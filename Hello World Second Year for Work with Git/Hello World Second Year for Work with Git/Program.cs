fd7daee176d3987dc250ffe2f674492058579751
Lesya Danko <lesyadankoit@gmail.com>
2024-10-14 13:05:15 -0400

    added spcialisation variable and PrintInfo method

--- Hello World Second Year for Work with Git/Hello World Second Year for Work with Git/Program.cs
+++ Hello World Second Year for Work with Git/Hello World Second Year for Work with Git/Program.cs
@@ -12,6 +12,11 @@ namespace Hello_World_Second_Year_for_Work_with_Git
             string name = Console.ReadLine();
             Console.WriteLine("What is your age");
             int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your specialization");
+            string specialization = Console.ReadLine();
+            ItGroup secondYear = new ItGroup( name, age, specialization);
+            secondYear.PrintInfo();
+
             Console.ReadKey();
         }
     }
